2409a159dd0cbf165ff9e2833969cd1d4f1a1cd4
mzerhusen <zerhusma@mail.uc.edu>
2025-03-07 21:41:28 -0500

    Update

--- src/BirthDateTime.java
+++ src/BirthDateTime.java
@@ -1,7 +1,53 @@
+import java.util.Scanner;
+
 public class BirthDateTime
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        int birthYear = 0;
+        int birthMonth = 0;
+        int birthDay = 0;
+        int birthHour = 0;
+        int birthMinute = 0;
+
+        birthYear = SafeInput.getRangedInt(input, "Please enter the year you were born", 1900, 2025);
+        birthMonth = SafeInput.getRangedInt(input, "Please enter the month you were born", 1, 12);
+        switch(birthMonth)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    birthDay = SafeInput.getRangedInt(input, "Please enter the day you were born", 1, 31);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    birthDay = SafeInput.getRangedInt(input, "Please enter the day you were born", 1, 30);
+                    break;
+                case 2:
+                    if(birthYear % 4 == 0 && birthYear % 100 != 0)
+                    {
+                        birthDay = SafeInput.getRangedInt(input, "Please enter the day you were born", 1, 29);
+                    }
+                    else if(birthYear % 100 == 0)
+                    {
+                        birthDay = SafeInput.getRangedInt(input, "Please enter the day you were born", 1, 28);
+                    }
+                    else;
+                    {
+                        birthDay = SafeInput.getRangedInt(input, "Please enter the day you were born", 1, 28);
+                    }
+                    break;
+            }
+        birthHour = SafeInput.getRangedInt(input, "Please enter the hour you were born in 24 hour time", 1, 24);
+        birthMinute = SafeInput.getRangedInt(input, "Please enter the minute you were born", 0, 59);
+        System.out.println("You said you were born at " + birthHour + ":" + birthMinute + ", on " + birthMonth + "/" + birthDay + "/" + birthYear + ".");
 
     }
 }
